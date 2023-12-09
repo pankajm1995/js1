3a0a17b63cddfb2cc1eaa31e385a3318c7111c82
pankajm1995 <153347011+pankajm1995@users.noreply.github.com>
2023-12-09 16:47:17 +0000

    datatypes

---
+++ .devcontainer/01_basic/02_datatype.js
@@ -0,0 +1,16 @@
+let name= "pankaj maurya"
+let age =29
+let isLoggedIn= true
+
+// data types
+// number
+// bigint
+// string=""
+// boolean=> true, false
+// null ==> stand alone value
+// undefined==> variable declare but not assign value
+// symbol==> unique
+// tc39.es for mdn
+// object 
+
+console.table([typeof(name),typeof(age),typeof(isLoggedIn),typeof(null)])
