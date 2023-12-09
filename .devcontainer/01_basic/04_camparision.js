366c583af7693ea6a44264a093baa0bae5344bd3
pankajm1995 <153347011+pankajm1995@users.noreply.github.com>
2023-12-09 21:15:43 +0000

    caparision done

---
+++ .devcontainer/01_basic/04_camparision.js
@@ -0,0 +1,23 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+
+
+// console.log("1">2)
+// console.log("2"<3)
+// console.log("5">=2)
+// console.log("5"==5)
+
+// console.log(null>0)
+// console.log(null<0)
+// console.log(null>=0)
+// console.log(null==0)
+
+
+// console.log(undefined==0)
+// console.log(undefined>0)
+// console.log(undefined>0)
+// console.log(undefined>=0)
+
+console.log("2"===2)
