b00f7e39b84bce1c3275ffd428c16274f3740dd9
pankajm1995 <153347011+pankajm1995@users.noreply.github.com>
2023-12-09 19:51:33 +0000

    conversion of datatype

---
+++ .devcontainer/01_basic/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score= undefined//30//true//null//"30abc"//"30"
+
+//console.log(typeof score);
+//console.log(typeof(score))
+
+let valueINnumber= Number(score)
+
+//console.log(typeof valueINnumber);
+//console.log(valueINnumber)
+
+/*
+"33"==>33
+"33abc"==>NaN
+true==>1, false ==>0
+undefined==>undefined
+30==>number
+*/
+
+let isloggedIn=1
+let booleanloggedin= Boolean(isloggedIn)
+//console.log(booleanloggedin);
+
+// 1==>true
+// 0==>false
+// ""==>false
+// "pankaj"==>true
+
+let somenumber =45
+let stringnumber= String(somenumber)
+console.log(stringnumber)
+console.log(typeof(stringnumber))
