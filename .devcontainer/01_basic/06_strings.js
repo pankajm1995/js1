4fad98d055a37ec6e3c3747e4b1a46aded91b416
pankajm1995 <153347011+pankajm1995@users.noreply.github.com>
2023-12-30 21:17:54 +0000

    commit

---
+++ .devcontainer/01_basic/06_strings.js
@@ -0,0 +1,25 @@
+let name = "Pankaj Maurya"
+let count=15
+
+console.log(name + count)
+
+console.log(`Hi my name is ${name} and i have repository count is ${count}`)
+
+const gameName = new String("Race and end")
+
+// console.log(gameName[5])
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase())
+
+// console.log(gameName.charAt(2)) // find 2nd index charcter
+
+// console.log(gameName.indexOf('c')) // find charcter position
+
+
+const newString= gameName.substring(0,4)
+
+console.log(newString)
---
+++ .devcontainer/01_basic/07_stackandheap.js
@@ -0,0 +1,9 @@
+//stack (primitive) declare variable ka copy milega
+let myname = "pankaj"
+let changename = myname
+
+changename="ashish"
+console.log(myname);
+console.log(changename);
+
+
